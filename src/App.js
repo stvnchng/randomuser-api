176596caef9d3ab3e4e30b160352abecda786458
stvnchng <sc111@rice.edu>
2021-05-17 14:27:28 -0500

    add code and dependencies to query from the randomuser api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [userInfo, setUserInfo] = useState([]);
+  const [pageNum, setPageNum] = useState(1);
+
+  useEffect(() => {
+    fetchAnotherUser();
+  }, [pageNum]);
+
+  const api = `https://api.randomuser.me/?page=${pageNum}`;
+
+  const paginate = () => {
+    setPageNum(pageNum + 1);
+  };
+
+  const fetchAnotherUser = () => {
+    fetchData().then((data) => {
+      const infos = [...userInfo, ...data.results];
+      setUserInfo(infos);
+    });
+  };
+
+  const fetchData = () => {
+    return axios
+      .get(api)
+      .then(({ data }) => data)
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const formatUserInfo = (user) => {
+    return `${user.name.first} ${user.name.last} ${user.email}`;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={paginate}>Get Another User</button>
+      {userInfo.map((user) => (
+        <>
+          <div>{formatUserInfo(user)}</div>
+          <img src={user.picture.large} alt="person" />
+        </>
+      ))}
     </div>
   );
 }
